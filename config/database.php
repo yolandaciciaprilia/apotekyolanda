9d00a78e5a0f00d1a1c4b4e8d9118357d72e04c2
username_anda <ciciapriliayolanda@gmail.com>
2020-08-18 16:15:31 +0700

    yolanda

---
+++ config/database.php
@@ -0,0 +1,17 @@
+
+
+<?php
+// deklarasi parameter koneksi database
+$server   = "localhost";
+$username = "root";
+$password = "";
+$database = "inven_apotik";
+
+// koneksi database
+$mysqli = new mysqli($server, $username, $password, $database);
+
+// cek koneksi
+if ($mysqli->connect_error) {
+    die('Koneksi Database Gagal : '.$mysqli->connect_error);
+}
+?>
